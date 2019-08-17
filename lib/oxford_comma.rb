e8112fd4e20bd681dbd9da067e0df2cf033d1e1d
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-17 05:53:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,10 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length == 3
-    return array.split(", ")
-  else
-    return array.join(", ")
+    last = array.pop
+    result = array.join(", ")
+    last = ", and " + last
+    result = result + last
+
   end
 end
