67c63991eff06af4443252161460041fb1083bea
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-17 05:56:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join("")
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length == 3
+    last = array.pop
+    result = array.join(", ")
+    last = ", and " + last
+    result = result + last
+  elsif array.length > 3
+    last = array.pop
+    result = array.join(", ")
+    last = ", and " + last
+    result = result + last
+    return result
+  end
 end
